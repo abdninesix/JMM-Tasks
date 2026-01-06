390269f5fb72e47a362009c4eaf5c6628ef76dd1
abdninesix <mohabd1995@gmail.com>
2026-01-06 12:42:16 +0500

    started day two task

--- day_two/src/App.jsx
+++ day_two/src/App.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <header>
+        <span><b>Email Us: </b>info@physio101.ca</span>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Locations</li>
+          <li>Contacy</li>
+          <li>Download Referral Form</li>
+        </ul>
+      </header>
+
+      <nav>
+        <img src="\101physio-logo.png" alt="logo" />
+        <ul></ul>
+        <button>Book Appointment</button>
+      </nav>
+      <section></section>
+      <footer></footer>
+    </div>
   )
 }
 
