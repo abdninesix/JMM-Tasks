0a41ef0e921c1bb160813aea1ec1bd50971416f8
abdninesix <mohabd1995@gmail.com>
2026-01-13 18:16:48 +0500

    Week 3 start

--- week3/practice.js
+++ week3/practice.js
@@ -1,9 +1,16 @@
-console.log("Hello, JavaScript!");
+console.log("Hello");
+
+
 
 let something = 100;
 
+
+
 var message = "Hi there!";
 console.log(message);
+
+
+
 
 let age = 25;
 let name = "Alice";
@@ -11,9 +18,16 @@ let alive = true;
 let nothing = null;
 let notAssigned;
 
+
+
+
 let x = 5 + 3;
 let isEqual = 10 == "10";
 let andCheck = (x > 5) && (x < 10);
+
+
+
+
 
 let score = 85;
 if (score >= 90) {
@@ -22,9 +36,15 @@ if (score >= 90) {
     console.log("Keep practicing");
 }
 
+
+
+
 for (var i = 0; i < 3; i++) {
     console.log("i", i);
 }
+
+
+
 
 var j = 0;
 while (j < 3) {
@@ -37,12 +57,21 @@ function greet(name) {
 }
 console.log(greet("Bob"));
 
+
+
+
 var colors = ["red", "green", "blue"];
 console.log(colors[0]);
 colors.push("yellow");
 
+
+
+
 var person = { name: "Ali", age: 30 };
 console.log(person.name);
+
+
+
 
 var globalVar = "I am global";
 function test() {
@@ -51,21 +80,36 @@ function test() {
 }
 test();
 
+
+
+
 let a = 5;
 let b = a;
 b = 10;
+
+
+
 
 let arr1 = [1, 2];
 let arr2 = arr1;
 arr2.push(3);
 
+
+
+
 let text = "hello";
 console.log(text.length);
 console.log(text.toUpperCase());
 
+
+
+
 let nums = [1, 2, 3];
 nums.push(4);
 nums.pop();
+
+
+
 
 try {
     console.log(notDefined);
