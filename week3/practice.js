06de09cce288a987d8548bdfb3dccd4c510faabf
abdninesix <mohabd1995@gmail.com>
2026-01-19 14:42:49 +0500

    updated

---
+++ week3/practice.js
@@ -0,0 +1,5 @@
+const now = new Date();
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+console.log("Today is: " + days[now.getDay()]);
+console.log("Current time is: " + now.toLocaleTimeString());
