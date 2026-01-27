05c0dd0d069c4a76f42ca6c185a3bd7e6dd31b94
abdninesix <mohabd1995@gmail.com>
2026-01-27 16:01:25 +0500

    updated

--- week4/practical/src/App.jsx
+++ week4/practical/src/App.jsx
@@ -1,22 +1,8 @@
 import React from 'react'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Form from './components/Form'
-import Table from './components/Table'
 
 const App = () => {
-
   return (
-    <>
-      <Header />
-      <Navbar />
-      <Hero />
-      <Footer />
-      <Form />
-      <Table />
-    </>
+    <div>App</div>
   )
 }
 
