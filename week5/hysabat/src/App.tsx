64741b9785cafad364e1a31b188789b96fb1316b
abdninesix <mohabd1995@gmail.com>
2026-01-29 22:06:59 +0500

    updated

--- week5/hysabat/src/App.tsx
+++ week5/hysabat/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/login/Login";
+import Login from "./pages/auth/Login";
 import DashboardLayout from "./components/layouts/DashboardLayout";
 
 export default function App() {
