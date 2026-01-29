6d39c98719235f4c8441ba556f43647a3510b1b3
abdninesix <mohabd1995@gmail.com>
2026-01-29 15:55:39 +0500

    updated

--- week5/hysabat/src/App.tsx
+++ week5/hysabat/src/App.tsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import Login from "./pages/Login";
 
-const App = () => {
+const DashboardLayout = () => (
+  <div className="flex h-screen">
+    <aside className="w-64 border-r">Sidebar</aside>
+    <main className="flex-1 p-8">
+      <Outlet />
+    </main>
+  </div>
+);
+
+export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        
+        {/* <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route index element={<Dashboard />} />
+        </Route> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
---
+++ week5/hysabat/src/pages/Login.tsx
@@ -0,0 +1,98 @@
+import { Input } from '@/components/ui/input'
+import { Lock, Mail, User } from 'lucide-react'
+import { useState } from 'react'
+import * as z from 'zod'
+
+const schema = z.object({
+    username: z.string().min(5, 'Username must be at least 5 characters'),
+    password: z.string().min(8, 'Password must be at least 8 characters'),
+})
+
+const Login = () => {
+
+    const [error, setError] = useState({})
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const result = schema.safeParse({
+            username: e.target.username.value,
+            password: e.target.password.value,
+        });
+
+        if (!result.success) {
+            setError(result.error.issues.reduce((acc, curr) => ({ ...acc, [curr.path[0]]: curr.message, }), {}));
+            return;
+        }
+
+        setError({});
+    };
+
+
+    return (
+        <div className='h-screen flex p-4 overflow-hidden'>
+            {/* Left section */}
+            <section className='h-full w-full lg:w-1/2 flex flex-col p-4 justify-between'>
+
+                <a href='/' className='w-30 lg:w-fit'>
+                    <img src='/logo2.svg' alt='logo' />
+                </a>
+
+                <form onSubmit={handleSubmit} className='flex flex-col items-center'>
+                    <div className='w-full md:max-w-md flex flex-col gap-4 text-gray-600'>
+
+                        <div className='space-y-2'>
+                            <h1 className='text-3xl font-bold text-black'>Log In</h1>
+                            <p>Login to get back to your Hysabat account.</p>
+                        </div>
+
+                        <div>
+                            <label htmlFor='username' className='text-sm font-bold'>Username<span className='text-red-600 ml-1'>*</span></label>
+                            <div className='flex items-center gap-2 p-2 border-2 border-gray-300 focus-within:border-sky-500 rounded-md'>
+                                <User className='text-gray-400' />
+                                <Input type='text' placeholder='Enter username' id='username' className='w-full outline-none' />
+                            </div>
+                            {/* {error.username && <p className='text-red-600 text-sm'>{error.username}</p>} */}
+                        </div>
+
+                        <div>
+                            <label htmlFor='password' className='text-sm font-bold'>Password<span className='text-red-600 ml-1'>*</span></label>
+                            <div className='flex items-center gap-2 p-2 border-2 border-gray-300 focus-within:border-sky-500 rounded-md'>
+                                <Lock className='text-gray-400' />
+                                <Input type='password' placeholder='Create a password' id='password' />
+                            </div>
+                            {/* {error.password && <p className='text-red-600 text-sm'>{error.password}</p>} */}
+                        </div>
+
+                        <button type='submit' className='bg-sky-500 hover:opacity-90 cursor-pointer text-white font-semibold py-2 rounded-lg'>Login</button>
+
+                        <p className='text-center text-sm'>
+                            Don't have an account? <a href='/#' className='text-sky-500'>Sign up</a>
+                        </p>
+                    </div>
+                </form>
+
+                <footer className='flex flex-col-reverse md:flex-row items-center justify-between gap-2 text-sm text-gray-600 font-semibold'>
+                    <p>&copy; Hysabat Solutions {new Date().getFullYear()}</p>
+                    <a href='mailto:sales@hysabat.com' className='flex gap-1'><Mail className='text-xl' /> sales@hysabat.com</a>
+                </footer>
+            </section>
+
+            {/* Right section */}
+            <section className='w-full lg:w-1/2 hidden lg:flex flex-col items-center p-20 justify-center bg-linear-to-br from-sky-400 to-blue-950 rounded-2xl'>
+                {/* Image */}
+                <div>
+                    <img src='/content.png' alt='content' className='scale-110 animate-slide' />
+                </div>
+                {/* Text */}
+                <div className='space-y-4 text-center text-white'>
+                    <h2 className='text-3xl font-semibold'>Transforming the Future of Resource Planning</h2>
+                    <p className='text-xl'>Empowering businesses with smarter resource management and real-time insights. HYSABAT ERP helps you optimize every resource, minimize waste, and drive operational efficiency. Gain full visibility into projects, anticipate resource needs, and respond swiftly to market changes.</p>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Login
+
