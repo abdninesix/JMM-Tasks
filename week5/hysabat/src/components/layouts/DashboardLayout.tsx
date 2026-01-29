d58c616c26d411b08b7e8e58ee38cfefb0f3cb6d
abdninesix <mohabd1995@gmail.com>
2026-01-29 20:54:14 +0500

    updated

--- week5/hysabat/src/components/layouts/DashboardLayout.tsx
+++ week5/hysabat/src/components/layouts/DashboardLayout.tsx
@@ -3,10 +3,11 @@ import { Outlet } from 'react-router-dom'
 const DashboardLayout = () => {
     return (
         <div className="flex h-screen">
-            <aside className="w-64 border-r">Sidebar</aside>
-            <main className="flex-1 p-8">
+            <aside className="w-64 border p-4">Sidebar</aside>
+            <main className="flex-1 border p-4">
                 <Outlet />
             </main>
+            <aside className='border'>Hi</aside>
         </div>
     )
 }
