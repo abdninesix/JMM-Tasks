a80148beb79a44ebebb17d766a1ed5f45dd14567
abdninesix <mohabd1995@gmail.com>
2026-02-03 21:24:21 +0500

    updated

--- week5/hysabat/src/pages/sales/sales-invoice/CreateSalesInvoice.tsx
+++ week5/hysabat/src/pages/sales/sales-invoice/CreateSalesInvoice.tsx
@@ -1,8 +1,8 @@
 import { AppBreadcrumb } from "@/components/AppBreadcrumb"
-import { Checkbox } from "@/components/ui/checkbox"
-import { Field, FieldContent, FieldLabel, FieldTitle } from "@/components/ui/field"
+import { Field, FieldLabel, FieldTitle } from "@/components/ui/field"
 import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Separator } from "@/components/ui/separator"
 
 const CreateSalesInvoice = () => {
@@ -24,24 +24,124 @@ const CreateSalesInvoice = () => {
 
       <Separator />
 
-      {/* Types */}
-      <div>
-        <Label className="py-4">Invoice Type</Label>
-        <RadioGroup defaultValue="plus" className="flex w-fit">
-          <FieldLabel htmlFor="plus-plan">
-            <Field orientation="horizontal">
-              <FieldTitle>Plus</FieldTitle>
-              <RadioGroupItem value="plus" id="plus-plan" />
-            </Field>
-          </FieldLabel>
-          <FieldLabel htmlFor="pro-plan">
-            <Field orientation="horizontal">
-              <FieldTitle>Pro</FieldTitle>
-              <RadioGroupItem value="pro" id="pro-plan" />
-            </Field>
-          </FieldLabel>
-        </RadioGroup>
-      </div>
+      {/* Inputs */}
+      <>
+        <>
+          <Label>Invoice Type</Label>
+          <RadioGroup defaultValue="tax" className="flex max-w-2xs">
+            <FieldLabel htmlFor="tax">
+              <Field orientation="horizontal">
+                <FieldTitle>Tax</FieldTitle>
+                <RadioGroupItem value="tax" id="tax" className=" text-white" />
+              </Field>
+            </FieldLabel>
+            <FieldLabel htmlFor="simplified-tax">
+              <Field orientation="horizontal">
+                <FieldTitle>Simplified Tax</FieldTitle>
+                <RadioGroupItem value="simplified-tax" id="simplified-tax" className=" text-white" />
+              </Field>
+            </FieldLabel>
+          </RadioGroup>
+        </>
+        <div className="flex flex-wrap gap-4 mt-6">
+          <Field className="w-fit">
+            <Label>Transaction Type</Label>
+            <Select >
+              <SelectTrigger>
+                <SelectValue placeholder="Select transaction type" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="apple">Apple</SelectItem>
+                <SelectItem value="banana">Banana</SelectItem>
+                <SelectItem value="blueberry">Blueberry</SelectItem>
+                <SelectItem value="grapes">Grapes</SelectItem>
+                <SelectItem value="pineapple">Pineapple</SelectItem>
+              </SelectContent>
+            </Select>
+          </Field>
+
+          <Field className="w-fit">
+            <Label>Transaction Type</Label>
+            <Select >
+              <SelectTrigger>
+                <SelectValue placeholder="Select transaction type" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="apple">Apple</SelectItem>
+                <SelectItem value="banana">Banana</SelectItem>
+                <SelectItem value="blueberry">Blueberry</SelectItem>
+                <SelectItem value="grapes">Grapes</SelectItem>
+                <SelectItem value="pineapple">Pineapple</SelectItem>
+              </SelectContent>
+            </Select>
+          </Field>
+
+          <Field className="w-fit">
+            <Label>Transaction Type</Label>
+            <Select >
+              <SelectTrigger>
+                <SelectValue placeholder="Select transaction type" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="apple">Apple</SelectItem>
+                <SelectItem value="banana">Banana</SelectItem>
+                <SelectItem value="blueberry">Blueberry</SelectItem>
+                <SelectItem value="grapes">Grapes</SelectItem>
+                <SelectItem value="pineapple">Pineapple</SelectItem>
+              </SelectContent>
+            </Select>
+          </Field>
+
+          <Field className="w-fit">
+            <Label>Transaction Type</Label>
+            <Select >
+              <SelectTrigger>
+                <SelectValue placeholder="Select transaction type" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="apple">Apple</SelectItem>
+                <SelectItem value="banana">Banana</SelectItem>
+                <SelectItem value="blueberry">Blueberry</SelectItem>
+                <SelectItem value="grapes">Grapes</SelectItem>
+                <SelectItem value="pineapple">Pineapple</SelectItem>
+              </SelectContent>
+            </Select>
+          </Field>
+
+          <Field className="w-fit">
+            <Label>Transaction Type</Label>
+            <Select >
+              <SelectTrigger>
+                <SelectValue placeholder="Select transaction type" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="apple">Apple</SelectItem>
+                <SelectItem value="banana">Banana</SelectItem>
+                <SelectItem value="blueberry">Blueberry</SelectItem>
+                <SelectItem value="grapes">Grapes</SelectItem>
+                <SelectItem value="pineapple">Pineapple</SelectItem>
+              </SelectContent>
+            </Select>
+          </Field>
+
+          <Field className="w-fit">
+            <Label>Transaction Type</Label>
+            <Select >
+              <SelectTrigger>
+                <SelectValue placeholder="Select transaction type" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="apple">Apple</SelectItem>
+                <SelectItem value="banana">Banana</SelectItem>
+                <SelectItem value="blueberry">Blueberry</SelectItem>
+                <SelectItem value="grapes">Grapes</SelectItem>
+                <SelectItem value="pineapple">Pineapple</SelectItem>
+              </SelectContent>
+            </Select>
+          </Field>
+        </div>
+      </>
+
 
     </div>
   )
