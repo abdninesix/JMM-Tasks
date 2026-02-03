9cf79344e8d9067e194c11c499931563cc8b3d36
abdninesix <mohabd1995@gmail.com>
2026-02-03 12:23:20 +0500

    updated

--- week5/hysabat/src/pages/sales/customers/columns.tsx
+++ week5/hysabat/src/pages/sales/customers/columns.tsx
@@ -1,9 +1,10 @@
 import type { ColumnDef } from "@tanstack/react-table"
-import { ArrowUpDown, Pen, Trash2 } from "lucide-react"
+import { ArrowUpDown, Copy, MoreHorizontal, Pencil, Trash2 } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Badge } from "@/components/ui/badge"
 import { Link } from "react-router-dom"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 
 type Customer = {
     id: string
@@ -103,7 +104,18 @@ export const columns: ColumnDef<Customer>[] = [
     },
     {
         accessorKey: "vatNumber",
-        header: "VAT Number"
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    className="has-[>svg]:px-0"
+                >
+                    VAT Number
+                    <ArrowUpDown className="size-3" />
+                </Button>
+            )
+        },
     },
     {
         accessorKey: "invoiceCount",
@@ -126,12 +138,31 @@ export const columns: ColumnDef<Customer>[] = [
     {
         id: "actions",
         header: "Actions",
-        cell: () => (
-            <>
-                <Button variant="ghost" size="icon" className="text-blue-500" ><Pen /></Button>
-                <Button variant="ghost" size="icon" className="text-destructive"><Trash2 /></Button>
-            </>
-        ),
+        cell: ({ row }) => {
+            const column = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem
+                            onClick={() => navigator.clipboard.writeText(column.id)}
+                        >
+                            <Copy />
+                            Copy customer ID
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem><Pencil className="text-blue-500" />Edit</DropdownMenuItem>
+                        <DropdownMenuItem><Trash2 className="text-destructive" />Delete</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
     },
 ]
 
