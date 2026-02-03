44b56f6e7594314eb30933fe489c6f9a6a5220b8
abdninesix <mohabd1995@gmail.com>
2026-02-03 12:56:55 +0500

    updated

--- week5/hysabat/src/pages/sales/sales-invoice/SalesInvoice.tsx
+++ week5/hysabat/src/pages/sales/sales-invoice/SalesInvoice.tsx
@@ -3,8 +3,12 @@ import { Button } from "@/components/ui/button"
 import { Kbd } from "@/components/ui/kbd"
 import { Separator } from "@/components/ui/separator"
 import { Plus } from "lucide-react"
+import { useNavigate } from "react-router-dom"
 
 const SalesInvoice = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div className="bg-card border rounded-md p-4 space-y-4">
       {/* Header Section */}
@@ -13,7 +17,7 @@ const SalesInvoice = () => {
         <div className="flex flex-col lg:flex-row justify-between gap-2">
           <h1 className="text-2xl font-semibold text-theme1">Sales Invoice</h1>
           <div className="flex items-center gap-4">
-            <Button className='text-white bg-theme1 hover:bg-theme1/90'>
+            <Button onClick={()=>navigate("/sales-invoice/create")} className='text-white bg-theme1 hover:bg-theme1/90'>
               <Plus />Create Invoice <Kbd className="bg-transparent text-white border">Ctrl+Alt+I</Kbd>
             </Button>
           </div>
