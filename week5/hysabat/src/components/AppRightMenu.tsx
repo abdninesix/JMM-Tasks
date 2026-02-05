07a24a4fe7a73deb5f680f1a7381606527dcdb5c
abdninesix <mohabd1995@gmail.com>
2026-02-05 15:02:12 +0500

    updated

---
+++ week5/hysabat/src/components/AppRightMenu.tsx
@@ -0,0 +1,15 @@
+import { Plus, ShoppingCart, Ticket } from "lucide-react"
+
+const AppRightMenu = () => {
+    return (
+        <aside className='h-dvh pl-2'>
+            <div className='sticky right-0 top-2/5 p-2 space-y-2 bg-card shadow-sm border rounded-l-xl'>
+                <Ticket size={28} className='cursor-pointer p-1 rounded-full text-white bg-theme1' />
+                <ShoppingCart size={28} className='cursor-pointer p-1 rounded-full text-white bg-theme1' />
+                <Plus size={28} className='cursor-pointer p-1 rounded-full text-white bg-theme1' />
+            </div>
+        </aside>
+    )
+}
+
+export default AppRightMenu
--- week5/hysabat/src/components/layouts/DashboardLayout.tsx
+++ week5/hysabat/src/components/layouts/DashboardLayout.tsx
@@ -2,23 +2,17 @@ import { Outlet } from 'react-router-dom'
 import { SidebarProvider } from '../ui/sidebar'
 import { AppSidebar } from '../AppSidebar'
 import Navbar from '../Navbar'
-import { Plus, ShoppingCart, Ticket } from 'lucide-react'
+import AppRightMenu from '../AppRightMenu'
 
 const DashboardLayout = () => {
     return (
-        <SidebarProvider defaultOpen={true} className='space-x-2'>
+        <SidebarProvider defaultOpen={true}>
             <AppSidebar />
             <main className="flex-1 flex flex-col gap-4 py-2 px-4 lg:px-2">
                 <Navbar />
                 <Outlet />
             </main>
-            <aside className='h-dvh'>
-                <div className='sticky right-0 top-2/5 p-2 space-y-2 bg-card shadow-sm border rounded-l-xl'>
-                    <Ticket size={28} className='cursor-pointer p-1 rounded-full text-white bg-theme1'/>
-                    <ShoppingCart size={28} className='cursor-pointer p-1 rounded-full text-white bg-theme1'/>
-                    <Plus size={28} className='cursor-pointer p-1 rounded-full text-white bg-theme1'/>
-                </div>
-            </aside>
+            <AppRightMenu />
         </SidebarProvider >
     )
 }
