81320f6cbc079c12ac60b4df1a70606296381c16
abdninesix <mohabd1995@gmail.com>
2026-03-10 19:19:54 +0500

    updated

--- week8+/testapp/app/Models/Post.php
+++ week8+/testapp/app/Models/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // The "fillable" property defines which columns can be 
+    // changed using the Post::create() or $post->update() methods.
+    protected $fillable = [
+        'title',
+        'description',
+        'author'
+    ];
 }
