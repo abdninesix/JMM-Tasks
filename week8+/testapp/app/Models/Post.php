6583368458068bfdf9dffdd4a41543848e98f01f
abdninesix <mohabd1995@gmail.com>
2026-03-10 19:45:29 +0500

    updated

--- week8+/testapp/app/Models/Post.php
+++ week8+/testapp/app/Models/Post.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    use HasFactory;
     // The "fillable" property defines which columns can be 
     // changed using the Post::create() or $post->update() methods.
     protected $fillable = [
