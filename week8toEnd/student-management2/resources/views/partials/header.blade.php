29d77ba0f4224517343006e9182af5c6bbb8ed06
abdninesix <mohabd1995@gmail.com>
2026-05-20 14:03:41 +0500

    Updated

--- week8toEnd/student-management2/resources/views/partials/header.blade.php
+++ week8toEnd/student-management2/resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header class="mb-8 flex items-center justify-between">
   <span class="text-2xl font-bold text-gray-800">Student Dashboard</span>
     <div class="flex gap-2 justify-center [&>a]:rounded [&>a]:px-2 [&>a]:py-1 [&>a]:font-semibold [&>a]:bg-gray-800 [&>a]:text-white">
-        <a href="{{ route('students.index') }}">Students</a>
+        <a href="{{ route('students.index') }}">Home</a>
         <a  href="{{ route('students.create') }}">Add New</a>
     </div>
 </header>
