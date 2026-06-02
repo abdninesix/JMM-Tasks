2b02331229708f83a3df0e0c48867c50bc4a5256
abdninesix <mohabd1995@gmail.com>
2026-06-02 09:46:56 +0500

    Updated

--- week8toEnd/auth-system/app/Models/User.php
+++ week8toEnd/auth-system/app/Models/User.php
@@ -18,9 +18,12 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'username',
+        'full_name',
         'email',
         'password',
+        'gender',
+        'dob',
     ];
 
     /**
