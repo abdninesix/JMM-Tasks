889f15611901977b449b29af33546bdf9e9baab0
abdninesix <mohabd1995@gmail.com>
2026-06-02 10:08:00 +0500

    Updated

--- week8toEnd/auth-system/app/Http/Controllers/Api/AuthController.php
+++ week8toEnd/auth-system/app/Http/Controllers/Api/AuthController.php
@@ -3,47 +3,28 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function register(UserRegisterRequest $request)
     {
-        //
-    }
+        $user = User::create([
+            'username' => $request->username,
+            'full_name' => $request->full_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'gender' => $request->gender,
+            'dob' => $request->dob,
+        ]);
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            'status' => true,
+            'message' => 'User registered successfully',
+            'user' => $user
+        ], 201);
     }
 }
