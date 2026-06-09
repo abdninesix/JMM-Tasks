4f243b306eb8be8f3523dbab8bf336cd0ada3443
abdninesix <mohabd1995@gmail.com>
2026-06-09 11:32:36 +0500

    Updated

--- week8toEnd/jwt-auth-system/app/Models/User.php
+++ week8toEnd/jwt-auth-system/app/Models/User.php
@@ -25,6 +25,7 @@ class User extends Authenticatable
         'password',
         'gender',
         'dob',
+        'role'
     ];
 
     /**
@@ -48,5 +49,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- week8toEnd/jwt-auth-system/routes/api.php
+++ week8toEnd/jwt-auth-system/routes/api.php
@@ -8,3 +8,14 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/me', [AuthController::class, 'me']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/refresh', [AuthController::class, 'refresh']);
+    });
+});
