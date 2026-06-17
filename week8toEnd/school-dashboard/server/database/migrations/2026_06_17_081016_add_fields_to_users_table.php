10cde6cc996a828bdb72c70cf26557f59fba1c1a
abdninesix <mohabd1995@gmail.com>
2026-06-17 13:12:26 +0500

    Updated

--- week8toEnd/school-dashboard/server/database/migrations/2026_06_17_081016_add_fields_to_users_table.php
+++ week8toEnd/school-dashboard/server/database/migrations/2026_06_17_081016_add_fields_to_users_table.php
@@ -4,15 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('phone')->nullable();
+            $table->string('profile_picture')->nullable();
         });
     }
 
