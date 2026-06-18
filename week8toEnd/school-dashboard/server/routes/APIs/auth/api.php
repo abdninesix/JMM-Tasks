1541f08ba3eb8b131c4fb589d1c1d39ed8899a53
abdninesix <mohabd1995@gmail.com>
2026-06-18 12:11:29 +0500

    Updated

--- week8toEnd/school-dashboard/server/routes/APIs/auth/api.php
+++ week8toEnd/school-dashboard/server/routes/APIs/auth/api.php
@@ -1,10 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
+
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
@@ -14,13 +14,5 @@ Route::prefix('auth')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::post('/refresh', [AuthController::class, 'refresh']);
     });
-});
 
-Route::middleware(['auth:api', 'isAdmin'])->group(function () {
-    Route::get('/admin/users', function () {
-        return response()->json([
-            'status' => 'success',
-            'users' => \App\Http\Resources\UserResource::collection(\App\Models\User::all())
-        ]);
-    });
 });
