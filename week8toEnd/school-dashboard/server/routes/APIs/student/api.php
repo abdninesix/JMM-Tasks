a77b5ec2460d806e2cdfd1734faaeeaca3dc9789
abdninesix <mohabd1995@gmail.com>
2026-06-18 12:09:06 +0500

    Updated

---
+++ week8toEnd/school-dashboard/server/routes/APIs/student/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:api')->group(function () {
+
+    Route::middleware('role:Student')->prefix('admin')->group(function () {
+        Route::get('/dashboard', function () {
+            return response()->json(['success' => true, 'message' => 'Welcome to Student Dashboard']);
+        });
+    });
+
+});
