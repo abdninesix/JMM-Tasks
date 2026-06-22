b8d85c036fc301d6f07100879063b6a10cbf6845
abdninesix <mohabd1995@gmail.com>
2026-06-22 16:10:09 +0500

    Updated

--- week8toEnd/school-dashboard/client/src/components/Navbar.jsx
+++ week8toEnd/school-dashboard/client/src/components/Navbar.jsx
@@ -23,22 +23,17 @@ const Navbar = () => {
 
                 <Link to="/about" className="rounded px-4 py-2 font-semibold ring duration-100 hover:bg-theme hover:text-white active:scale-90">About</Link>
 
-                {user?.role === "Admin" &&
-                    <Link to="/admin/users" className="rounded bg-theme px-4 py-2 font-semibold text-white duration-100 hover:bg-theme/80 active:scale-90">All Users</Link>
-                }
-
-
                 {user ?
                     <>
-                        <Link to="/dashboard" className="rounded bg-theme px-4 py-2 font-semibold text-white duration-100 hover:bg-theme/80 active:scale-90">Dashboard</Link>
-
                         <button onClick={() => setProfileMenu(prev => !prev)} className="w-10 h-10 cursor-pointer rounded-full bg-theme text-white flex items-center justify-center text-lg font-bold overflow-hidden">
                             {!user?.profile_picture ? user?.full_name?.split(" ").map(word => word.charAt(0).toUpperCase()).join("") : <img src={user?.profile_picture} className='object-cover size-full' />}
                         </button>
                         {profileMenu && (
-                            <div className='absolute top-16 right-8 p-2 bg-white shadow-md flex flex-col gap-2'>
+                            <div className='absolute top-16 right-8 p-2 bg-white rounded-md border border-gray-200 flex flex-col gap-2'>
+                                <Link to="/dashboard" className="font-semibold">Dashboard</Link>
+                                {user?.role === "Admin" && <Link to="/admin/users" className="font-semibold">Users</Link>}
                                 <Link to="/profile" className="font-semibold">Profile</Link>
-                                <button onClick={handleSignout} className="cursor-pointer text-red-500 font-semibold">Sign Out</button>
+                                <button onClick={handleSignout} className="text-left cursor-pointer text-red-500 font-semibold">Sign Out</button>
                             </div>
                         )}
                     </>
