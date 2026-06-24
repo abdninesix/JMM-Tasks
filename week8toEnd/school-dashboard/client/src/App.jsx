147e1de1f654adb9428c428a465da5adad737aa4
abdninesix <mohabd1995@gmail.com>
2026-06-24 14:27:14 +0500

    Updated

--- week8toEnd/school-dashboard/client/src/App.jsx
+++ week8toEnd/school-dashboard/client/src/App.jsx
@@ -3,20 +3,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Navbar from './components/Navbar'
 import { ToastContainer } from 'react-toastify'
-import Home from './pages/Home'
-import About from './pages/About'
-import Register from './pages/auth/Register'
-import Login from './pages/auth/Login'
-import ForgotPassword from './pages/auth/ForgotPassword'
-import ResetPassword from './pages/auth/ResetPassword'
 import { AuthProvider } from './context/AuthContext'
-import Profile from './pages/profile/Profile'
-import Users from './pages/admin/Users'
 import GuestRoute from './components/redirects/GuestRoute'
 import ProtectedRoute from './components/redirects/ProtectedRoute'
-import AdminDashboard from './pages/admin/Dashboard'
-import TeacherDashboard from './pages/teacher/Dashboard'
-import StudentDashboard from './pages/student/Dashboard'
 import RoleRedirect from './components/redirects/RoleRedirect'
 import { adminRoutes, guestRoutes, protectedRoutes, publicRoutes, studentRoutes, teacherRoutes } from './utils/routes'
 
@@ -38,30 +27,14 @@ const App = () => {
             ))}
 
             {/* Protected */}
-            {protectedRoutes.map(({ path, element }) => (
-              <Route key={path} path={path} element={<ProtectedRoute>{element}</ProtectedRoute>} />
-            ))}
-
-            {/* Admin */}
-            {adminRoutes.map(({ path, element }) => (
-              <Route key={path} path={path} element={<ProtectedRoute allowedRole="Admin">{element}</ProtectedRoute>} />
-            ))}
-
-            {/* Teacher */}
-            {teacherRoutes.map(({ path, element }) => (
-              <Route key={path} path={path} element={<ProtectedRoute allowedRole="Teacher">{element}</ProtectedRoute>} />
-            ))}
-
-            {/* Student */}
-            {studentRoutes.map(({ path, element }) => (
-              <Route key={path} path={path} element={<ProtectedRoute allowedRole="Student">{element}</ProtectedRoute>} />
+            {protectedRoutes.map(({ path, element, role }) => (
+              <Route key={path} path={path} element={<ProtectedRoute allowedRole={role}>{element}</ProtectedRoute>} />
             ))}
 
             {/* Public */}
             {publicRoutes.map(({ path, element }) => (
               <Route key={path} path={path} element={element} />
             ))}
-
 
           </Routes>
         </BrowserRouter>
--- week8toEnd/school-dashboard/client/src/utils/routes.jsx
+++ week8toEnd/school-dashboard/client/src/utils/routes.jsx
@@ -1,84 +1,39 @@
 import Home from "../pages/Home";
 import About from "../pages/About";
-
 import Register from "../pages/auth/Register";
 import Login from "../pages/auth/Login";
 import ForgotPassword from "../pages/auth/ForgotPassword";
 import ResetPassword from "../pages/auth/ResetPassword";
-
 import Profile from "../pages/profile/Profile";
-
 import AdminDashboard from "../pages/admin/Dashboard";
 import Users from "../pages/admin/Users";
-
 import TeacherDashboard from "../pages/teacher/Dashboard";
-
 import StudentDashboard from "../pages/student/Dashboard";
-
 import RoleRedirect from "../components/redirects/RoleRedirect";
 
 export const guestRoutes = [
-    {
-        path: "/register",
-        element: <Register />,
-    },
-    {
-        path: "/login",
-        element: <Login />,
-    },
-    {
-        path: "/forgot-password",
-        element: <ForgotPassword />,
-    },
-    {
-        path: "/reset-password",
-        element: <ResetPassword />,
-    },
+    { path: "/register", element: <Register />, },
+    { path: "/login", element: <Login />, },
+    { path: "/forgot-password", element: <ForgotPassword />, },
+    { path: "/reset-password", element: <ResetPassword />, },
 ];
 
 export const protectedRoutes = [
-    {
-        path: "/",
-        element: <Home />,
-    },
-    {
-        path: "/dashboard",
-        element: <RoleRedirect />,
-    },
-    {
-        path: "/profile",
-        element: <Profile />,
-    },
-];
+    { path: "/", element: <Home />, },
+    { path: "/dashboard", element: <RoleRedirect />, },
+    { path: "/profile", element: <Profile />, },
 
-export const adminRoutes = [
-    {
-        path: "/admin/dashboard",
-        element: <AdminDashboard />,
-    },
-    {
-        path: "/admin/users",
-        element: <Users />,
-    },
-];
+    // Admin routes
+    { path: "/admin/dashboard", element: <AdminDashboard />, role: "Admin" },
+    { path: "/admin/users", element: <Users />, role: "Admin" },
 
-export const teacherRoutes = [
-    {
-        path: "/teacher/dashboard",
-        element: <TeacherDashboard />,
-    },
-];
+    // Teacher routes
+    { path: "/teacher/dashboard", element: <TeacherDashboard />, role: "Teacher" },
 
-export const studentRoutes = [
-    {
-        path: "/student/dashboard",
-        element: <StudentDashboard />,
-    },
+    // Student routes
+    { path: "/student/dashboard", element: <StudentDashboard />, role: "Student" },
 ];
 
 export const publicRoutes = [
-    {
-        path: "/about",
-        element: <About />,
-    },
+    { path: "/about", element: <About />, },
 ];
